e85feecd78d62e7a40647460c6641a5db1ac1837
Danilo Toriy <randomlittlest@gmail.com>
2026-03-24 22:22:03 -0300

    feat: adicionando o repositório de Projeto

---
+++ src/main/java/com/example/danilo/repository/ProjetoRepository.java
@@ -0,0 +1,7 @@
+package com.example.danilo.repository;
+
+import com.example.danilo.entity.Projeto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjetoRepository extends JpaRepository<Projeto, Long> {
+}
